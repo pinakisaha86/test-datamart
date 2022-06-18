3e458ff1636463d08c3483e542a7fa5581233def
chait <chaitanya.td99@gmail.com>
2022-06-18 09:53:16 +0530

    test

--- com/test/source_data_loading.py
+++ com/test/source_data_loading.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets_test")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
