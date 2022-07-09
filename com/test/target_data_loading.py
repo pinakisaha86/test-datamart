db43025c2b3c07e3a1c2e490f5def778dad1c4ea
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-09 10:29:14 +0530

    test

---
+++ com/test/target_data_loading.py
@@ -0,0 +1,33 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+import yaml
+import os.path
+from com.utility.utils import *
+
+
+if __name__ == '__main__':
+
+    os.environ["PYSPARK_SUBMIT_ARGS"] = (
+        '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
+    )
+# Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    s3_bucket = app_conf['s3_conf']['s3_bucket']
+    staging_loc = app_conf['s3_conf']['staging_loc']
+
+    src_list = app_conf['tgt_list']
