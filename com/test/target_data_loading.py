839ea7c92ae8afe02ea2a5bd277e7481cdcb1905
pinaki Saha <pinaki.saha86@gmail.com>
2022-07-23 12:34:39 +0530

    test

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -48,4 +48,23 @@ if __name__ == '__main__':
                 .option("dbtable", "public.REGIS_DIM") \
                 .mode("overwrite") \
                 .save()
+
+        elif tgt == 'CHILD_DIM':
+            child_dim_df = spark.read.parquet('s3a://' + s3_bucket + '/' + staging_loc + '/' + tgt_conf['source_data'])
+
+            child_dim_df.createOrReplaceTempView("CP")
+            result1 = spark.sql(tgt_conf['loadingQuery'])
+
+           # result.repartition(1).write.option("header", "true").mode("overwrite").parquet("")
+            jdbc_url = get_redshift_jdbc_url(app_secret)
+            print(jdbc_url)
+
+            result1.coalesce(1).write \
+                .format("io.github.spark_redshift_community.spark.redshift") \
+                .option("url", jdbc_url) \
+                .option("tempdir", "s3a://" + s3_bucket + "/temp") \
+                .option("forward_spark_s3_credentials", "true") \
+                .option("dbtable", "public.CHILD_DIM") \
+                .mode("overwrite") \
+                .save()
 # spark-submit --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" com/test/target_data_loading.py
